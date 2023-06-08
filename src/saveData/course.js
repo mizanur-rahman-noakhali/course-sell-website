d72d31e8ab9fca4e3a06e3b1c91c25e4d137518d
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-06-08 16:02:17 +0600

    first commit

---
+++ src/saveData/course.js
@@ -0,0 +1,15 @@
+let course =[
+  {"key":"1","name":"python Bootcamp zero to hero","price":"10"},
+  {"key":"2","name":"python Programming","price":"20"},
+  {"key":"3","name":"Matching Larning ","price":"30"},
+  {"key":"4","name":"master Programming","price":"40"},
+  {"key":"5","name":"Data Science","price":"70"},
+  {"key":"6","name":"network programming","price":"110"},
+  {"key":"7","name":"web development","price":"200"},
+  {"key":"8","name":"java scripts","price":"250"},
+  {"key":"9","name":"react","price":"180"},
+  {"key":"10","name":"php","price":"90"},
+  {"key":"11","name":"my sql",'price':"120"},
+  {"key":"12","name":"java","price":"12"}
+
+]
